1271e5c483d1658405009f306162dcb9607578f6
krazijames <krazijames@gmail.com>
2021-12-22 00:57:20 +0900

    Split test files

---
+++ test/mock.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <doctest/trompeloeil.hpp>
+
+template <typename State, typename Action> class Mock
+{
+public:
+    MAKE_MOCK1(action_listener, void(Action));
+    MAKE_MOCK1(state_listener, void(State));
+    MAKE_MOCK2(reducer, State(State, Action));
+};
